dfeecc3abc56e09590dfba1fbf23c9fd51033939
nickBarak <nicholasjbarak@gmail.com>
2020-08-15 22:39:32 -0700

    .

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const next = require('next');
 const express = require('express');
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 
 app.prepare().then(() => {
-  const server = express();
+    const server = express();
 
-  server.use(function (req, res, next) {
+    server.use(expressCspHeader({
+        directives: {
+            'default-src': [SELF],
+            'script-src': [SELF, INLINE, 'https://mocknexus.com'],
+            'worker-src': [NONE],
+            'block-all-mixed-content': true
+        }
+    }));
+
+    server.use(function(req, res, next) {
     if (req.header('x-forwarded-proto') === 'http') {
-      res.redirect(301, 'https://' + req.hostname + req.url);
-      return
+        res.redirect(301, 'https://' + req.url);
+        return
     }
     next()
-  });
+    });
 
-  server.listen(port, err => {
+    server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
-  });
+    });
 });
