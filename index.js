2632f0cc35d11312d4fc6d217fb443c34d230195
nickBarak <nicholasjbarak@gmail.com>
2020-08-16 15:38:28 -0700

    .

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const port = process.env.PORT || 3000;
 const express = require('express');
-const { client } = require('./URLs');
 
 app.prepare().then(_=> {
     const server = express();
-    !dev && server.use( (req, res) => (req.protocol === 'http') && res.redirect(client) );
+    !dev && server.use( (req, res) => (req.protocol === 'http') && res.redirect('https://www.mocknexus.com') );
     server.listen(port, _=> console.log('Listening on port', port))
 });
