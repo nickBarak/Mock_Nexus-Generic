866cb1b155c27e18719e2268ffb4dc9232d5bff2
nickBarak <nicholasjbarak@gmail.com>
2020-08-16 18:54:51 -0700

    .

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const port = process.env.PORT || 3000;
+const handle = app.getRequestHandler();
 const express = require('express');
+const port = process.env.PORT || 3000;
 
 /* Redirect HTTP to HTTPS */
 app.prepare().then(_=> {
     const server = express();
     server.enable('trust proxy');
-    !dev && server.use( (req, res) => !req.secure && res.redirect('https://www.mocknexus.com') );
+    
+    server.use( (req, res) => {
+        if (req.secure || dev) { handle(req, res, parse(req.url, true)) }
+        else return res.redirect('https://www.mocknexus.com');
+    });
+
     server.listen(port, _=> console.log('Listening on port', port))
 });
